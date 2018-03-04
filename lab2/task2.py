701b7cc0d50d22759c242aa78cf80f04dbc578b8
oskam <oska.magdalena@gmail.com>
2018-03-04 18:51:11 +0100

    comments add

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,18 +1,25 @@
 import numpy as np
 import scipy.stats
 
+# binomial distribution
+# pmf(x) = (n choose x) * p^x * (1-p)^(n-x)
+# where:
+# n - number of trails
+# p - probability of success
+# x - number of successes
 
 x = 0
 p = 0.1
 n = 9
 
-#1 numpy
-print(sum(np.random.binomial(9, 0.1, 20000) == 0)/20000)
+# 1 numpy
+print(sum(np.random.binomial(9, 0.1, 20000) == 0) / 20000)
 
-#2 scipy
+# 2 scipy
 print(scipy.stats.binom.pmf(x, n, p))
 
-#3 me
+
+# 3 me
 def binomial_coefficient(n, x):
     if x == 0:
         return 1
@@ -21,7 +28,9 @@ def binomial_coefficient(n, x):
     res = binomial_coefficient(n - 1, x) + binomial_coefficient(n - 1, x - 1)
     return res
 
+
 def binomial_distribution(n, p, x):
-    return binomial_coefficient(n, x)*(p**x)*((1-p)**(n-x))
+    return binomial_coefficient(n, x) * (p ** x) * ((1 - p) ** (n - x))
+
 
 print(binomial_distribution(n, p, x))
