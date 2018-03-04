2deaba61f7a360d43fd7fc258a2e2f8561929963
oskam <oska.magdalena@gmail.com>
2018-03-04 22:17:17 +0100

    nope

--- lab2/task3.py
+++ lab2/task3.py
@@ -1,28 +1,27 @@
 import numpy as np
 import scipy
-import scipy.special
+from scipy.special import factorial
 
 
-faculties_prob = [.02, .05, .15, .1, .15, .1, .3, .07, .06]
+probs = [.02, .05, .15, .1, .15, .1, .3, .07, .06]
 
-prob = [1-x for x in faculties_prob]
+ # 1 (Poisson binomial distribution)
+prob = [1-x for x in probs]
 result = np.prod(np.array(prob))
 print(result)
 
-#1.
-# jednokrotne powtórzenie eksperymentu o X+1 możliwych rezultatach,
-# gdzie potencjalne rezultaty: 0,1..,X oznaczają oznaczają liczbę kierunków,
-# na które student się dostał.
-# Pr(Y_1 = 1, Y_2 = 0,....,Y_(X+1) = 0 )  =
-# = 1!/[n_1!*...*n_(X+1)!]  * (P_1)^1* (P_2)^0 * .... *(P_(X+1))^0 =
-# = 1!/1! * P_1 = Pr(Y_1 = 0) = (1- p_1)*...*(1-p_X)
-
-
-result = 1!/
-
-print(scipy.special.factorial(5))
-def multinomial(x,n,p):
-    result = scipy.special.factorial(n)/((scipy.special.factorial(1)*scipy.special.factorial(2))) * p**0
-    return result
+# 2
+# x = 9 # liczba kierunkow
+# num_of_successes = 0 # na ile się dostał
 #
-print(multinomial(x,n,p))
+# ns = np.zeros(x, dtype=int)
+# ns[num_of_successes] = 1
+# print(ns)
+#
+# ns_fact = np.prod([factorial(n) for n in ns])
+#
+# probs_to_ns = np.prod([(prob ** n) for prob, n in zip(probs, ns)])
+#
+# total_prob = (1 / ns_fact) * probs_to_ns
+#
+# print(total_prob)
