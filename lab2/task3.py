6930e49a526421b07d66c9e4257d007d0c2d7e2f
oskam <oska.magdalena@gmail.com>
2018-03-04 10:20:56 +0100

    start fixing

--- lab2/task3.py
+++ lab2/task3.py
@@ -3,14 +3,22 @@ import scipy
 import scipy.special
 
 
-faculties_prop = [.02, .05, .15, .1, .15, .1, .3, .07, .06]
+faculties_prob = [.02, .05, .15, .1, .15, .1, .3, .07, .06]
 
-prob = [1-x for x in faculties_prop]
+prob = [1-x for x in faculties_prob]
 result = np.prod(np.array(prob))
 print(result)
 
-n = 1
+#1.
+# jednokrotne powtórzenie eksperymentu o X+1 możliwych rezultatach,
+# gdzie potencjalne rezultaty: 0,1..,X oznaczają oznaczają liczbę kierunków,
+# na które student się dostał.
+# Pr(Y_1 = 1, Y_2 = 0,....,Y_(X+1) = 0 )  =
+# = 1!/[n_1!*...*n_(X+1)!]  * (P_1)^1* (P_2)^0 * .... *(P_(X+1))^0 =
+# = 1!/1! * P_1 = Pr(Y_1 = 0) = (1- p_1)*...*(1-p_X)
 
+
+result = 1!/
 
 print(scipy.special.factorial(5))
 def multinomial(x,n,p):
